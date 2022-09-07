3d083ac1bf063f0cf3526cadeb6fd1d185ac06eb
peytontolbert <peytonct@gmail.com>
2022-09-07 16:27:50 -0400

    maybe fix pages

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/engineer', function(request, response) {
 	response.sendFile(path.join(__dirname + '/public/indexengineer.html'));
