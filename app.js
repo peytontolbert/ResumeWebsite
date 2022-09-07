58eec85388bf477090ff471359b1dd9175dfc35e
peytontolbert <peytonct@gmail.com>
2022-09-07 08:19:41 -0400

    al

--- app.js
+++ app.js
@@ -7,30 +7,18 @@ var app = express();
 
 var server = http.createServer(app);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'Application')));
+app.use(express.static('public'));
 
 app.get('/engineer', function(request, response) {
-	response.sendFile(path.join(__dirname + '/indexengineer.html'));
+	response.sendFile((__dirname + '/public/indexengineer.html'));
 });
 
 app.get('/devops', function(request, response) {
-	response.sendFile(path.join(__dirname + '/indexdevops.html'));
+	response.sendFile(path.join(__dirname + '/public/indexdevops.html'));
 });
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/Application/index.html'));
+	response.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 server.listen(process.env.PORT || 443); 
